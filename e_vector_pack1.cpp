90786cb1bcd9c7bde0af131fda8fbe1d1e8f0fee
Esper77 <yo.moyo12355@gmail.com>
2023-02-05 17:26:47 +0300

    Added pack1

---
+++ e_vector_pack1.cpp
@@ -0,0 +1,39 @@
+#include "easy_list.h"
+
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
+    for (int x = 0; x < mass.size(); x += 2)
+        mass2.push_back(mass[x]);
+}
+
+
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
+    for (int x = 0; x < mass.size(); x++)
+        if (mass[x] % 2 == 0) 
+        mass2.push_back(mass[x]);
+}
+
+
+int itc_positive_list(const vector <int> &mass){
+    int counter = 0;
+    for (int x = 0; x < mass.size(); x++)
+        if (mass[x] < 0) 
+        counter++;
+    return counter;
+}
+
+
+int itc_sl_list(const vector <int> &mass){
+    int counter = 0;
+    for (int x = 1; x < mass.size(); x++)
+        if (mass[x-1] < mass[x])
+        counter++;
+    return counter;
+}
+
+bool itc_same_parts_list(const vector <int> &mass){
+    bool flag;
+    for (int x = 1; x < mass.size(); x++)
+        if ((mass[x-1] >= 0) == (mass[x] >= 0))
+        flag = 1;
+    return flag;
+}
--- easy_list.h
+++ easy_list.h
@@ -1,5 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 #ifndef EASY_LIST_H_INCLUDED
 #define EASY_LIST_H_INCLUDED
+
+// pack1
 
 void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);
 
@@ -11,12 +17,14 @@ int itc_sl_list(const vector <int> &mass);
 
 bool itc_same_parts_list(const vector <int> &mass);
 
+// pack2
+
 void itc_rev_list(vector <int> &mass);
 
 void itc_rev_par_list(vector <int> &mass);
 
 void itc_rshift_list(vector <int> &mass);
 
-void itc_super_shift_list(vector <int> &mass, int n;
+void itc_super_shift_list(vector <int> &mass, int n);
 
 #endif // EASY_LIST_H_INCLUDED
