b4628dcc82e1d5baacec709f197c5555089067e2
Esper77 <yo.moyo12355@gmail.com>
2023-03-13 20:27:29 +0300

    Patched a bunch of funcs

--- m_vector_pack1.cpp
+++ m_vector_pack1.cpp
@@ -11,8 +11,11 @@ vector <char> itc_strtlist(string str){
 
 string itc_join(vector <char> lst, string sep){
     string str = "";
-    for (int i = 0; i < lst.size(); i++)
+    str += lst[0]
+    for (int i = 1; i < lst.size(); i++) {
+        str += sep;
         str += lst[i];
+        }
     return str;
 }
 
@@ -23,6 +26,7 @@ string itc_rmstrspc(string str){
     for (int i = 0; i < len; i++)
         if (str[i] != ' ')
             new_str += str[i];
+    return new_str;
 }
 
 string itc_rmstrchar(string str, string less){
@@ -34,6 +38,7 @@ string itc_rmstrchar(string str, string less){
         for (int x = 0; x < len_remove; x++)
             if ((str[i] != ' ') && (less[x] != str[i]))
                 new_str += str[i];
+    return neew_str;
 }
 
  long itc_sumlst(const vector <int> &lst){
